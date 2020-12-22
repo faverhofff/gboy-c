fb65c2e78143734010f84ad7f2dc7744ad579adc
Frank Averhoff <faverhofff@gmail.com>
2020-12-22 12:15:46 -0500

    Downgrade imgui

--- debugger.cpp
+++ debugger.cpp
@@ -1,6 +1,5 @@
 #include "imgui.h"
 #include "imgui_impl_sdl.h"
-#include "imgui_impl_opengl3.h"
 #include <stdio.h>
 #include <SDL.h>
 #include <SDL_opengl.h>
@@ -9,26 +8,19 @@
 
 Debugger::Debugger() 
 {
-	const char* glsl_version = "#version 130";
-
-	SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL);
+	SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI);
 	mWindow = SDL_CreateWindow("Dear ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
 	mContext = SDL_GL_CreateContext(mWindow);
 
-	SDL_GL_SetSwapInterval(1); // Enable vsync
 	SDL_GL_MakeCurrent(mWindow, mContext);
-	ImGui::CreateContext();
+	SDL_GL_SetSwapInterval(1); // Enable vsync
 
-	ImGui::StyleColorsDark();
-
-	ImGui_ImplSDL2_InitForOpenGL(mWindow, mContext);
-	ImGui_ImplOpenGL3_Init(glsl_version);
+	ImGui_ImplSdl_Init(mWindow);
 }
 
 Debugger::~Debugger()
 {
-	ImGui_ImplSDL2_Shutdown();
-	ImGui::DestroyContext();
+	ImGui_ImplSdl_Shutdown();
 	
 	SDL_GL_DeleteContext(mContext);
 	SDL_DestroyWindow(mWindow);
@@ -36,18 +28,14 @@ Debugger::~Debugger()
 
 void Debugger::HandleEvent(SDL_Event& _event)
 {
-	ImGui_ImplSDL2_ProcessEvent(&_event);
+	ImGui_ImplSdl_ProcessEvent(&_event);
 }
 
 void Debugger::Render()
 {
-	IMGUI_CHECKVERSION();
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
-
-	ImGui_ImplOpenGL3_NewFrame();
-	ImGui_ImplSDL2_NewFrame(mWindow);
-	ImGui::NewFrame();
-
+	ImGui_ImplSdl_NewFrame(mWindow);
+	//ImGui::NewFrame();
 
 	// TODO: all controls
 	glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
@@ -55,5 +43,6 @@ void Debugger::Render()
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	ImGui::Render();
+
 	SDL_GL_SwapWindow(mWindow);
 }
--- gboy.cpp
+++ gboy.cpp
@@ -1,7 +1,7 @@
 #include <imgui.h>
 #include <imgui_impl_sdl.h>
-#include <imgui_impl_opengl3.h>
 #include <SDL.h>
+#include <SDL_opengl.h>
 
 #include <iostream>
 #include <stdio.h>
@@ -12,44 +12,27 @@
 #include "debugger.h"
 
 
-
-// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
-// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
-// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
-#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
-#pragma comment(lib, "legacy_stdio_definitions")
-#endif
-
 int main(int argn, char *argc[])
 {
-	// GL 3.0 + GLSL 130
-	const char* glsl_version = "#version 130";
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-
-
-	// Setup SDL
-	// (Some versions of SDL before <2.0.10 appears to have performance/stalling issues on a minority of Windows systems,
-	// depending on whether SDL_INIT_GAMECONTROLLER is enabled or disabled.. updating to latest version of SDL is recommended!)
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 	{
 		cout << "Error: " << SDL_GetError();
-		return -1;
+		exit;
 	}
 
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+
 
 	Debugger debugger;
 	SDL_Event event;
 	bool done = false;
-
+	
 	while (true) {
+		
 		while (SDL_PollEvent(&event))
 		{
 			debugger.HandleEvent(event);
@@ -57,7 +40,7 @@ int main(int argn, char *argc[])
 			if (event.type == SDL_QUIT)
 				done = true;
 		}
-
+		
 		debugger.Render();
 	}
 
@@ -68,6 +51,6 @@ int main(int argn, char *argc[])
     gb.PowerUp(argc[1], argc[2]);
 	*/
 
-    return 0;
+    //return 0;
 }
 
